8cdfe91b49222ce44c150225d3e2252a02dd0b7e
Rachel Brindle <you@subluminal.net>
2015-03-11 19:48:05 -0700

    Initial Commit

---
+++ ProceduralKit/ProceduralKit.h
@@ -0,0 +1,19 @@
+//
+//  ProceduralKit.h
+//  ProceduralKit
+//
+//  Created by Rachel Brindle on 3/11/15.
+//  Copyright (c) 2015 Rachel Brindle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProceduralKit.
+FOUNDATION_EXPORT double ProceduralKitVersionNumber;
+
+//! Project version string for ProceduralKit.
+FOUNDATION_EXPORT const unsigned char ProceduralKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProceduralKit/PublicHeader.h>
+
+
